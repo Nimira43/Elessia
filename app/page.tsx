6f3ebb109579284f5576e6b478a9ac7c3a47444b
Nimira43 <n.rabone72@aol.com>
2025-06-06 03:32:35 +0100

    Installed React Icons. Update Shadcn theme correctly this time for TW3. TEsting out Tailwind, Shadcn and icons - all good.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { BiSolidCat } from 'react-icons/bi'
+
 function Home() {
   return (
-    <div>
-      <h1>Elessia</h1>
-    </div>
+    <>
+      <div className='flex bg-dark p-10'>
+        <BiSolidCat className='text-4xl text-light' />
+        <h1 className='logo text-4xl text-light'>Elessia</h1>
+      </div>
+      <div className='p-10 mt-10'>
+        <Button className='uppercase'>Login</Button>
+      </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
   theme: {
   	extend: {
   		colors: {
-  			main: '#ff4500',
-  			support: '#ff4500',
-  			dark: '#111',
-  			light: '#fffcfa',
+  			'main': '#ff4500',
+  			'support': '#008080',
+  			'dark': '#111',
+  			'light': '#fffcfa',
   			'grey-dark': '#333',
   			'grey-medium': '#888',
   			'grey-light': '#ccc',
