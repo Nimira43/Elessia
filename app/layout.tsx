7563fa359fec508a0710ef3f2908fd2b7b8d587b
Nimira43 <n.rabone72@aol.com>
2025-06-06 05:00:49 +0100

    Setting up basic navbar with links to login and register pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,6 +21,23 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
+        <nav className='bg-dark text-light p-5 h-24 flex items-center justify-between'>
+          <Link href='/'>
+            Elessia
+          </Link>
+          <ul>
+            <li>
+              <Link href='/login'>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link href='/register'>
+                Register
+              </Link>
+            </li>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-import { Button } from '@/components/ui/button'
-import { BiSolidCat } from 'react-icons/bi'
-
 function Home() {
   return (
-    <>
-      <div className='flex bg-dark p-10'>
-        <BiSolidCat className='text-4xl text-light' />
-        <h1 className='logo text-4xl text-light'>Elessia</h1>
-      </div>
-      <div className='p-10 mt-10'>
-        <Button className='uppercase'>Login</Button>
-      </div>
-    </>
+    <div>
+      Home Page
+    </div>
   )
 }
 
