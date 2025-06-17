3636392b0df11f8523cdba157fa5f68458f5ba57
Nimira43 <n.rabone72@aol.com>
2025-06-17 16:02:43 +0100

    Updated continue-with-google-button.tsx

--- components/continue-with-google-button.tsx
+++ components/continue-with-google-button.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { Button } from './ui/button'
-import { auth } from '@/firebase/client'
+import { useAuth } from '@/context/auth'
 
 export default function ContinueWithGoogleButton() {
+  const auth = useAuth()
   return (
     <Button
       onClick={() => {
- 
+        auth?.loginWithGoogle()
       }}
     >
       Continue with Google
