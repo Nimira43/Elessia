2759463afcb524f9396d20eabdc571538ceff3ba
Nimira43 <n.rabone72@aol.com>
2025-06-17 19:23:43 +0100

    Auth tokens- 6

--- context/actions.ts
+++ context/actions.ts
@@ -2,6 +2,7 @@ import { admin } from 'firebase-admin';
 'use server'
 
 import { auth } from '@/firebase/server'
+import { cookies } from 'next/headers';
 
 export const setToken = async ({
   token,
@@ -12,15 +13,29 @@ export const setToken = async ({
 }) => {
   try {
     const verifiedToken = await auth.verifyIdToken(token)
+    
     if (!verifiedToken) {
       return
     }
+    
     const userRecord = await auth.getUser(verifiedToken.uid)
+    
     if (process.env.ADMIN_EMAIL === userRecord.email && !userRecord.customClaims?.admin) {
       auth.setCustomUserClaims(verifiedToken.uid, {
         admin: true
       })
     }
+
+    const cookieStore = await cookies()
+    cookieStore.set('firebaseAuthToken', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production'
+    })
+    cookieStore.set('firebaseAuthRefreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production'
+    })
+
   } catch (e) {
     console.log(e)
   }
