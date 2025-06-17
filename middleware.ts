a00e3b9439b19af619b67cdf9d34920c9b64e75d
Nimira43 <n.rabone72@aol.com>
2025-06-17 21:15:32 +0100

    Middleware -3

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
+import { decodeJwt } from 'jose'
 
 export async function middleware(request: NextRequest) {
   console.log('Middleware: ', request.url)
@@ -8,6 +9,12 @@ export async function middleware(request: NextRequest) {
   }
   const cookieStore = await cookies()
   const token = cookieStore.get('firebaseAuthToken')
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  const decodedToken = decodeJwt(token)
 }
 
 export const config = {
