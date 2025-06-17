de4bac63eb020b186c1318cb5062236b6ee87609
Nimira43 <n.rabone72@aol.com>
2025-06-17 19:16:15 +0100

    Auth tokens -4

--- context/actions.ts
+++ context/actions.ts
@@ -1,3 +1,4 @@
+import { admin } from 'firebase-admin';
 'use server'
 
 import { auth } from '@/firebase/server'
@@ -10,7 +11,16 @@ export const setToken = async ({
   refreshToken: string
 }) => {
   try {
-    const verifiedToken = await auth.verifyIdToken(token)  
+    const verifiedToken = await auth.verifyIdToken(token)
+    if (!verifiedToken) {
+      return
+    }
+    const userRecord = await auth.getUser(verifiedToken.uid)
+    if (process.env.ADMIN_EMAIL === userRecord.email && !userRecord.customClaims?.admin) {
+      auth.setCustomUserClaims(verifiedToken.uid, {
+        admin: true
+      })
+    }
   } catch (e) {
     console.log(e)
   }
