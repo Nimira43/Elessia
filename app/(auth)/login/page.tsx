541ab3ad8acfb56c19d30d9d7c601737361ea8af
Nimira43 <n.rabone72@aol.com>
2025-06-17 17:16:02 +0100

    Created (auth)/layout.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,19 +3,17 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function Login() {
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle
-            className='font-medium uppercase'
-          >
-            Login
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ContinueWithGoogleButton />
-        </CardContent>
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle
+          className='font-medium uppercase'
+        >
+          Login
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ContinueWithGoogleButton />
+      </CardContent>
+    </Card>
   )
 }
