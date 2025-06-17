e1561c7ca178879081ea5a5964b6e02a2a908615
Nimira43 <n.rabone72@aol.com>
2025-06-17 21:10:11 +0100

    Middleware - 2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
-import { NextRequest } from 'next/server'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   console.log('Middleware: ', request.url)
+  if (request.method === 'POST') {
+    return NextResponse.next()
+  }
+  const cookieStore = await cookies()
+  const token = cookieStore.get('firebaseAuthToken')
 }
 
 export const config = {
