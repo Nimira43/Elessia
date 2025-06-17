6e778058855ddba1caafe5fae5ca10934ca01908
Nimira43 <n.rabone72@aol.com>
2025-06-17 15:09:45 +0100

    AuthProvider - 2

--- context/auth.tsx
+++ context/auth.tsx
@@ -7,5 +7,13 @@ const AuthContext = createContext(null)
 export const AuthProvider = ({ children }: {
   children: React.ReactNode
 }) => {
+  return (
+    
 
+    <AuthContext.Provider
+      value={}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
