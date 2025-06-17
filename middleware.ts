51f0304d452e90482dcfb52c1d276e60cd57a81f
Nimira43 <n.rabone72@aol.com>
2025-06-17 21:04:59 +0100

    Setting up protected root

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 import { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  console.log('Middleware: ', request.url)
+}
 
+export const config = {
+  matcher: [
+    '/admin-dashboard'
+  ],
 }
