cd2d586a4612a3d8a382e1dba0e5aef256ebce78
Nimira43 <n.rabone72@aol.com>
2025-06-17 17:21:17 +0100

    Added TW classes to wrapper div in layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,7 @@ export default function Layout({children}: {
   children: React.ReactNode
 }) {
   return (
-    <div>
-      Auth
+    <div className='max-w-screen-sm mx-auto p-5'>
       {children}
     </div>
   )
