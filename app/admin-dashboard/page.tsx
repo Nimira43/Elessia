c352748695c1310bd4ac66e42eba228b44083a2e
Nimira43 <n.rabone72@aol.com>
2025-06-28 14:14:49 +0100

    Headline and link

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,13 +1,23 @@
 import { Breadcrumbs } from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 export default function AdminDashboard() {
   return (
     <div>
-      <Breadcrumbs 
+      <Breadcrumbs
         items={[{
           label: 'Dashboard'
         }]}
       />
+      <h1 className='text-4xl font-medium mt-6'>Admin Dashboard</h1>
+      <Button asChild>
+        <Link href='/admin-dashboard/new'>
+          <AiOutlinePlus />
+          New Property
+        </Link>
+      </Button>
     </div>
   )
 }
