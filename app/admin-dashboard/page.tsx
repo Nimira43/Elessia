6f6ae5fde947acbf287329f1142f3560af8f88bf
Nimira43 <n.rabone72@aol.com>
2025-06-28 14:07:32 +0100

    Brought in BreadCrumbs into AdminDashboard

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { Breadcrumbs } from '@/components/ui/breadcrumb'
+
 export default function AdminDashboard() {
   return (
-    <div>Admin Dashboard</div>
+    <div>
+      <Breadcrumbs 
+        items={[{
+          label: 'Dashboard'
+        }]}
+      />
+    </div>
   )
 }
