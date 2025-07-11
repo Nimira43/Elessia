bf3bfea3fcb0e95e4837076901f4b624c8c27d25
Nimira43 <n.rabone72@aol.com>
2025-07-11 19:52:44 +0100

    Adding more fields to propertyDataSchema

--- validation/propertySchema.ts
+++ validation/propertySchema.ts
@@ -3,17 +3,24 @@ import z from 'zod'
 export const propertyDataSchema = z.object({
   address1: z
     .string()
-    .min(1, 'Address line 1 must contain a value.'),
+    .min(1, 'This field must contain a value.'),
   address2: z
     .string()
     .optional(),
   city: z
     .string()
-    .min(3, 'City must contain at least 3 characters.'),
+    .min(3, 'City must contain at least three characters.'),
   postcode: z
     .string()
     .refine((postcode) => {
       const postcodeRegex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s?[0-9][A-Z]{2}%/i
-    })
-
+      return postcodeRegex.test(postcode)
+    }, 'Invalid UK postcode'),
+  price: z.coerce
+    .number()
+    .positive('Price must be greater than zero'),
+  description: z
+    .string()
+    .min(40, 'Description must containe at least forty characters'),
+   
 })
