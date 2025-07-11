b644e291c10f68db6ca0ab96b5aafa2cc26bab37
Nimira43 <n.rabone72@aol.com>
2025-07-11 19:34:22 +0100

    Containing work on propertyDataSchema

--- validation/propertySchema.ts
+++ validation/propertySchema.ts
@@ -1,5 +1,19 @@
 import z from 'zod'
 
 export const propertyDataSchema = z.object({
+  address1: z
+    .string()
+    .min(1, 'Address line 1 must contain a value.'),
+  address2: z
+    .string()
+    .optional(),
+  city: z
+    .string()
+    .min(3, 'City must contain at least 3 characters.'),
+  postcode: z
+    .string()
+    .refine((postcode) => {
+      const postcodeRegex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s?[0-9][A-Z]{2}%/i
+    })
 
 })
