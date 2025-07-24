3abd4b15adf1c6e0d4f392f8c5ec0a22ec110c15
Nimira43 <n.rabone72@aol.com>
2025-07-24 03:54:55 +0100

    Setting type for form

--- components/property-form.tsx
+++ components/property-form.tsx
@@ -2,9 +2,10 @@
 
 import { useForm } from 'react-hook-form'
 import { propertyDataSchema } from '@/validation/propertySchema'
+import z from 'zod'
 
 export default function PropertyForm() {
-  const form = useForm()
+  const form = useForm<z.infer<typeof propertyDataSchema>>()
   return (
     <div>
       Property Form
