1b51d55866e6a71248affb081ca3792cb7a54a85
Nimira43 <n.rabone72@aol.com>
2025-07-24 03:53:56 +0100

    Imported propertyDataSchema

--- components/property-form.tsx
+++ components/property-form.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useForm } from 'react-hook-form'
-/
+import { propertyDataSchema } from '@/validation/propertySchema'
+
 export default function PropertyForm() {
   const form = useForm()
   return (
