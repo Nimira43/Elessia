deccb49b228015329d4cce027eaf1cd6aff9e597
Nimira43 <n.rabone72@aol.com>
2025-07-24 23:11:44 +0100

    Added text and icon content to the Button prop

--- app/admin-dashboard/new/new-property-form.tsx
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -3,14 +3,22 @@
 import PropertyForm from '@/components/property-form'
 import { propertyDataSchema } from '@/validation/propertySchema'
 import z from 'zod'
-
+import { AiOutlinePlus } from 'react-icons/ai'
 
 export default function NewPropertyForm() {
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {}
 
   return (
     <div>
-      <PropertyForm handleSubmit={handleSubmit}/>
+      <PropertyForm 
+        handleSubmit={handleSubmit}
+        submitButtonLabel={
+          <>
+            <AiOutlinePlus />
+            Create Property
+          </>
+        }
+      />
     </div>
   )
 }
