bce1486b84dc6917e23035555cfc635b581c2034
Nimira43 <n.rabone72@aol.com>
2025-08-07 23:01:33 +0100

    actions - 2

--- app/admin-dashboard/new/actions.ts
+++ app/admin-dashboard/new/actions.ts
@@ -1,3 +1,13 @@
 'use server'
 
-export const saveNewProperty = async () => {}
+export const saveNewProperty = async (data : {
+  address1: string
+  address2?: string
+  city: string
+  postcode: string
+  description: string
+  price: number
+  bedrooms: number
+  bathrooms: number
+  status: 'draft' | 'for sale' | 'withdrawn' | 'sold'
+}) => {}
