dfac658dd544bb9b111316e741bb07c2e059d6cc
Nimira43 <n.rabone72@aol.com>
2025-08-07 23:18:19 +0100

    handleSubmit - 1

--- app/admin-dashboard/new/new-property-form.tsx
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -4,9 +4,13 @@ import PropertyForm from '@/components/property-form'
 import { propertyDataSchema } from '@/validation/propertySchema'
 import z from 'zod'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { useAuth } from '@/context/auth'
 
 export default function NewPropertyForm() {
+  const auth = useAuth()
+
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
+    const token = await auth?.currentUser?.getIdToken()
     console.log(data)
   }
 
