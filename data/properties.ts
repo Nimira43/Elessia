b5a82fd3654942214b8ed8d9410e7b53d5f851db
Nimira43 <n.rabone72@aol.com>
2025-08-26 21:44:03 +0100

    Creating getPropertiesOptions type

--- data/properties.ts
+++ data/properties.ts
@@ -1,5 +1,14 @@
 import 'server-only'
 
-export const getProperties = async () => {
-  
+type getPropertiesOptions = {
+  filters?: {
+    minPrice?: number | null 
+    maxPrice?: number | null 
+    minBedrooms?: number | null 
+    status?: number | null 
+  }
+}
+
+export const getProperties = async (options?: GetPropertiesOptions) => {
+
 }
