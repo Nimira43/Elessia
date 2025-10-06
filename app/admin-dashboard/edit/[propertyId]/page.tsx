e3898462300faf14f846c9e94eab6b973a3bbc77
Nimira43 <n.rabone72@aol.com>
2025-10-06 20:58:17 +0100

    Card markup

--- app/admin-dashboard/edit/[propertyId]/page.tsx
+++ app/admin-dashboard/edit/[propertyId]/page.tsx
@@ -1,5 +1,7 @@
 import { Breadcrumbs } from '@/components/ui/breadcrumb'
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { getPropertyById } from '@/data/properties'
+import NewPropertyForm from '../../new/new-property-form'
 
 export default async function EditProperty({
   params
@@ -13,16 +15,26 @@ export default async function EditProperty({
   return (
     <div>
       <Breadcrumbs 
-        items={[{
+        items={[
+          {
           href: '/admin-dashboard',
           label: 'Dashboard'
-        }, {
-          label: 'Edit Property'
-        }]}
-      >
-
-      </Breadcrumbs>
-
+          }, 
+          {
+            label: 'Edit Property'
+          }
+        ]}
+      />
+      <Card className='mt-5'>
+        <CardHeader>
+          <CardTitle className='text-3xl font-medium uppercase'>
+            Edit Property
+          </CardTitle>
+        </CardHeader>
+        <CardContent> 
+          EditPropertyForm
+        </CardContent>
+      </Card>
     </div>
   )
 }
