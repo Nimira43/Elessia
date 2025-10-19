bf15fd6cd0c68206990b88515766a507d7cb0a35
Nimira43 <n.rabone72@aol.com>
2025-10-19 09:56:01 +0100

    EditPropertyPage - 2

--- app/admin-dashboard/edit/[propertyId]/edit-property-form.tsx
+++ app/admin-dashboard/edit/[propertyId]/edit-property-form.tsx
@@ -5,6 +5,7 @@ import { Property } from '@/types/property'
 type Props = Property
 
 export default function EditPropertyForm({
+  id, address1,address2, city, postcode, bathrooms, bedrooms, description, price, status
 
 }: Props) {
   return (
