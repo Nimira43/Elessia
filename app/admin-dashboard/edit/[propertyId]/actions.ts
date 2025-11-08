92446d1b23aeb33daf8af1994465c7d57582d71c
Nimira43 <n.rabone72@aol.com>
2025-11-08 19:46:23 +0000

    updateProperty - 2

--- app/admin-dashboard/edit/[propertyId]/actions.ts
+++ app/admin-dashboard/edit/[propertyId]/actions.ts
@@ -1,5 +1,22 @@
 'use server'
 
-export const updateProperty = async () => {
-  
+import { Property } from '@/types/property'
+
+export const updateProperty = async (data: Property, authToken: string) => {
+  const verifiedToken = await auth.verifyIdToken(token)
+
+  if (!verifiedToken.admin) {
+    return {
+      error: true,
+      message: 'Unauthorised.'
+    }
+  }
+
+  const validation = propertyDataSchema.safeParse(propertyData)
+  if (!validation.success) {
+    return {
+      error: true,
+      message: validation.error.issues[0]?.message ?? 'An error occurred.'
+    }
+  }
 }
