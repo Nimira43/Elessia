2d86ddb4749791e881851e26130bfa9940457d43
Nimira43 <n.rabone72@aol.com>
2025-12-11 06:14:46 +0000

    MultiImageUploader updated - 1

--- components/ui/multi-image-uploader.tsx
+++ components/ui/multi-image-uploader.tsx
@@ -22,6 +22,7 @@ export default function MultiImageUploader({
   return (
     <div className='w-full max-w-3xl mx-auto p-4'>
       <input
+        ref={uploadInputRef}
         type='file'
         multiple
         accept='images/*'
