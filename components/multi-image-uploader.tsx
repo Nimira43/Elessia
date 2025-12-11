83844798fb57445261208f31493268a3ea4264f0
Nimira43 <n.rabone72@aol.com>
2025-12-11 06:26:19 +0000

    Hiding file input. Changing variant of Upload Images button.

--- components/multi-image-uploader.tsx
+++ components/multi-image-uploader.tsx
@@ -22,6 +22,7 @@ export default function MultiImageUploader({
   return (
     <div className='w-full max-w-3xl mx-auto p-4'>
       <input
+        className='hidden'
         ref={uploadInputRef}
         type='file'
         multiple
@@ -29,6 +30,7 @@ export default function MultiImageUploader({
       />
       <Button
         type='button'
+        variant='outline'
         onClick={
           () => uploadInputRef?.current?.click()
         }
