2e8b367e89d05364e09b9178dd28970f3669484f
Nimira43 <n.rabone72@aol.com>
2026-02-04 08:37:58 +0000

    Time to let this one go. Numerous issues have made this project a problem one. Purged the more recent commits where the code problems occurred but this needs a complete reset. Will start again under a new project title called Phoenix. However this repo and Elessia is retired.

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApps } from 'firebase/app'
 import { Auth, getAuth } from 'firebase/auth'
 import { FirebaseStorage, getStorage } from 'firebase/storage'
 
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
